1d26396fdbc6a51174b4bd31ec2a52c520742273
Ernie Eunsang Lee <ernie.eunsang@gmail.com>
2016-05-19 07:46:52 +0900

    CSS linting 추가, PostCSS 연결, Rucksack 테스트 완료

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,37 @@ var webpack = require('webpack')
 var path = require('path')
 
 var ExtractTextPlugin = require("extract-text-webpack-plugin")
+var rucksack = require('rucksack-css')
 
 module.exports = {
+  // Entry
   context: path.join(__dirname, 'src'),
-  entry: "entry.js",
+  entry: "./entry.js",
+
+  // Output
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: "bundle.js"
+    filename: "./bundle.js"
   },
+
+  // Loaders
   module: {
     loaders: [
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader')
       }
     ]
   },
+
+  // PostCSS Configurations
+  postcss: [
+    rucksack({
+      autoprefixer: true
+    })
+  ],
+
+  // Plugins
   plugins: [
     new ExtractTextPlugin("bundle.css", { allChunks: true })
   ]
